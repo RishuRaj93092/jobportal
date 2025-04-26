e2beca205df4cdcc41edaf967b5d40d659b6535d
Rishu Raj <2024sl93092@wilp.bits-pilani.ac.in>
2025-04-26 19:58:11 +0530

    initial setup

---
+++ src/main/java/com/jobportal/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.jobportal.controller;
+
+import com.jobportal.entity.User;
+import com.jobportal.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        return userService.loginUser(user.getEmail(), user.getPassword());
+    }
+}
